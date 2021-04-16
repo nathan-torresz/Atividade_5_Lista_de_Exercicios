4db93bcd3b5fa91c10939d29b206e8a54e2cda81
nathan-torresz <81251140+nathan-torresz@users.noreply.github.com>
2021-04-15 22:17:35 -0300

    Projeto iniciado

---
+++ Atividade_5_Lista/Atividade_5_Lista/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Atividade_5_Lista
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
